2c1bcf9ab2ec5bbb68c4a565fd1a6a78643f5b8c
paper <1246299550@qq.com>
2024-04-10 09:31:26 +0000

    first learning

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,8 +3,14 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+    let res = add(1, 2);
+    println!("res:{}", res);
+}
+
+fn call_me() {}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
