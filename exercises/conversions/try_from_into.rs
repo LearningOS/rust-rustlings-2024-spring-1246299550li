89125147bf627ae9bf45afa373af5a030019e2fc
paper <1246299550@qq.com>
2024-04-25 08:10:42 +0000

    0425 done

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,23 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn cov(v: i16) -> Result<u8, IntoColorError> {
+    if v < 0 || v > 255 {
+        Err(IntoColorError::IntConversion)
+    } else {
+        Ok(v as u8)
+    }
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: cov(tuple.0)?,
+            green: cov(tuple.1)?,
+            blue: cov(tuple.2)?,
+        })
     }
 }
 
@@ -48,6 +59,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: cov(arr[0])?,
+            green: cov(arr[1])?,
+            blue: cov(arr[2])?,
+        })
     }
 }
 
@@ -55,6 +71,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            Ok(Color {
+                red: cov(slice[0])?,
+                green: cov(slice[1])?,
+                blue: cov(slice[2])?,
+            })
+        }
     }
 }
 
