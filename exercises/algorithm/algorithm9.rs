c4125ba211e406f8140401101961499b0c9612d8
paper <1246299550@qq.com>
2024-04-28 13:43:53 +0000

    0428 done

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,8 +1,7 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,7 +36,17 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.count += 1;
+        self.items.push(value);
+
+        let mut idx = self.count;
+        while self.parent_idx(idx) > 0 {
+            let parent_idx = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                self.items.swap(idx, parent_idx);
+            }
+            idx = parent_idx;
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +66,20 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left_child_index = self.left_child_idx(idx);
+        let right_child_index = self.right_child_idx(idx);
+        if right_child_index > self.count {
+            left_child_index
+        } else {
+            if (self.comparator)(
+                &self.items[left_child_index],
+                &self.items[right_child_index],
+            ) {
+                left_child_index
+            } else {
+                right_child_index
+            }
+        }
     }
 }
 
@@ -84,8 +105,22 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+        let next = Some(self.items.swap_remove(1));
+        self.count -= 1;
+        if self.count > 0 {
+            let mut idx = 1;
+            while self.children_present(idx) {
+                let c_idx = self.smallest_child_idx(idx);
+                if !(self.comparator)(&self.items[idx], &self.items[c_idx]) {
+                    self.items.swap(idx, c_idx);
+                }
+                idx = c_idx;
+            }
+        }
+        next
     }
 }
 
